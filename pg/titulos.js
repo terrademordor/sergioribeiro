a399c39c3f396dd5c667dc20817b35cf6877890a
terrademordor <57503164+terrademordor@users.noreply.github.com>
2019-12-10 18:11:51 +0000

    Atualização
    Atualização

---
+++ pg/titulos.js
@@ -0,0 +1,8 @@
+	var xpto = document.getElementById("pt");
+	//df = 5;
+	df = "Loja de Batatas";
+	xpto.innerHTML = df;
+
+	var xpto = document.getElementById("br");
+	df1 = "Brasil no seu melhor";
+	xpto.innerHTML = df1;
